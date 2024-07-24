9fe0f0074562588c55b1e91fc77c015c4548ad25
Felipe <felipe.carrasco.villegas@gmail.com>
2024-07-23 22:09:54 -0400

    Hito 3 comppletado

--- onlyflans/web/views.py
+++ onlyflans/web/views.py
@@ -1,11 +1,44 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import ContactForm, Flan
+from .forms import ContactFormForm, ContactFormModelForm
 
 def indice(request):
-    return render(request, 'index.html', {})
+    public_flans = Flan.objects.filter(is_private=False)
+    return render(
+        request,
+        'index.html', 
+        {
+            'public_flans': public_flans
+        }
+)
 
 def acerca(request):
     return render(request, 'about.html', {})
 
 def bienvenido(request):
-    return render(request, 'welcome.html', {})
+    private_flans = Flan.objects.filter(is_private=True)
+    return render(
+        request,
+        'welcome.html', 
+        {
+            'private_flans': private_flans
+        }
+)
+
+def contacto(request):
+    if request.method == 'POST':
+        form = ContactFormModelForm(request.POST)
+
+        if form.is_valid():
+            contact_form = ContactForm.objects.create(**form.cleaned_data)
+    
+            return HttpResponseRedirect('/exito')
+    else:
+        form = ContactFormModelForm()
+
+    return render(request, 'contactus.html', {'form': form})
+
+def exito(request):
+    return render(request, 'success.html', {})
 
